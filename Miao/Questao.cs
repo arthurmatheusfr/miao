79598a0b5022629d316ccb2417bc5bbd434a8cc5
arthur <arthurmatheusfreitas@gmail.com>
2024-09-17 08:48:23 -0300

    oi

--- Miao/Questao.cs
+++ Miao/Questao.cs
@@ -1,11 +1,38 @@
 namespace Miao;
 
 public class Questao;
+public string RespostaErrada {get; set;}
+public string RespostaCorreta {get; set;}
+public string pergunta {get; set;}
+private button QualBTN(int rr)
 {
-public string resposta1 {get; set;}
-
-protected int resposta2 {get; set;}
-
-public string resposta3 {get; set;}
-
+    if(rr == 1)
+    return BotaoResposta01;
+    else if (rr == 2)
+    return BotaoResposta02;
+    else if (rr == 3)
+    return BotaoResposta03;
+    else if (rr == 4)
+    return BotaoResposta04;
+    else if (rr == 5)
+    return BotaoResposta05;
+    else
+    return null;
+}
+public bool VerificaResposta(int rr)
+{
+    if (RespostaCerta == rr)
+    {
+        var BTN = QualBTN(rr);
+        BTN BackgroundColor = Colors.Green;
+        return true;
+    }
+    else
+    {
+        var BTNCorreto = QualBTN(RespostaCerta);
+        var BTNIncorreto = QualBTN(rr);
+        BTNCorreto.BackgroundColor = Colors.Yellow;
+        BTNIncorreto.BackgroundColor = Colors.Red;
+        return false;
+    }
 }
