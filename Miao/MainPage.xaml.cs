a005fbef454d6dc88836f21c27bd9d3f85011e65
arthur <arthurmatheusfreitas@gmail.com>
2024-09-19 10:39:15 -0300

    oi

--- Miao/MainPage.xaml.cs
+++ Miao/MainPage.xaml.cs
@@ -2,6 +2,32 @@
 
 public partial class MainPage : ContentPage
 {
-	
+	Gerenciador gerenciador;
+    public MainPage()
+    {
+        InitializeComponent();
+        gerenciador = new Gerenciador(labelPergunta, BTNResposta01, BTNResposta02, BTNResposta03, BTNResposta04, BTNResposta05);
+        gerenciador = ProximaPergunta();
+    }
+    void OnBTNResp01Clicked(object sender, EventArgs args)
+    {
+        Gerenciador.VerificaCorreto(1);
+    }
+    void OnBTNResp02Clicked(object sender, EventArgs args)
+    {
+        Gerenciador.VerificaCorreto(2);
+    }
+    void OnBTNResp03Clicked(object sender, EventArgs args)
+    {
+        Gerenciador.VerificaCorreto(3);
+    }
+    void OnBTNResp04Clicked(object sender, EventArgs args)
+    {
+        Gerenciador.VerificaCorreto(4);
+    }
+    void OnBTNResp05Clicked(object sender, EventArgs args)
+    {
+        Gerenciador.VerificaCorreto(5);
+    }
 }
 
