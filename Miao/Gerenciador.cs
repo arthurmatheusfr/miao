84a28301f7e4c9a0b4f2bf5eb974812cc50727ab
arthur <arthurmatheusfreitas@gmail.com>
2024-09-23 09:00:30 -0300

    ioioioioioi

--- Miao/Gerenciador.cs
+++ Miao/Gerenciador.cs
@@ -2,6 +2,18 @@ namespace Miao;
 
 public class Gerenciador
 {
+  public int Pontuacao {get; private set;}
+  int NivelAtual = 0;
+  void Inicializar()
+  {
+    Pontuacao = 0;
+    NivelAtual = 0;
+    ProximaQuestao();
+  }
+  void AdicionaPontuacao(int N)
+  {
+
+  }
     List<Questao> ListaQuestoes = new List<Questao>();
     List<int> ListaQuestoesRespondidas = new List<int>();
     Questao QuestaoCorrente;
@@ -22,6 +34,36 @@ public class Gerenciador
         Q1.ConfigurarDesenho(labelPergunta, BTNResposta01, BTNResposta02, BTNResposta03, BTNResposta04, BTNResposta05);
         ListaQuestoes.Add(Q1);
         var Q2 = new Questao();
+        Q2.pergunta = " Qual é a capital da França?";
+        Q2.Resposta01 = "Madrid";
+        Q2.Resposta02 = "Roma";
+        Q2.Resposta03 = "Paris";
+        Q2.Resposta04 = "Londres";
+        Q2.Resposta05 = "Berlim";
+        Q2.RespostaCorreta = 3;
+        Q2.ConfigurarDesenho(labelPergunta, BTNResposta01, BTNResposta02, BTNResposta03, BTNResposta04, BTNResposta05);
+        ListaQuestoes.Add(Q2);
+       var Q3 = new Questao();
+        Q3.pergunta = " Quem é o autor de A Moreninha?";
+        Q3.Resposta01 = "Machado de Assis";
+        Q3.Resposta02 = "Joaquim Manuel de Macedo";
+        Q3.Resposta03 = " Álvares de Azevedo";
+        Q3.Resposta04 = "José de Alencar";
+        Q3.Resposta05 = "Graciliano Ramos";
+        Q3.RespostaCorreta = 2;
+        Q3.ConfigurarDesenho(labelPergunta, BTNResposta01, BTNResposta02, BTNResposta03, BTNResposta04, BTNResposta05);
+        ListaQuestoes.Add(Q3);
+       var Q4 = new Questao();
+       Q4.pergunta = " Quem escreveu O Pequeno Príncipe?";
+        Q4.Resposta01 = "Antoine de Saint-Exupéry";
+        Q4.Resposta02 = "J.K. Rowling";
+        Q4.Resposta03 = "Mark Twain";
+        Q4.Resposta04 = "Lewis Carroll";
+        Q4.Resposta05 = "C.S. Lewis";
+        Q4.RespostaCorreta = 2;
+        Q4.ConfigurarDesenho(labelPergunta, BTNResposta01, BTNResposta02, BTNResposta03, BTNResposta04, BTNResposta05);
+        ListaQuestoes.Add(Q4);
+       var Q5 = new Questao();
     }
       public void ProximaQuestao()
       {
@@ -37,7 +79,14 @@ public class Gerenciador
         if(QuestaoCorrente.VerificaResposta(rr))
         {
          await Task.Delay(1000);
+         AdicionaPontuacao(NivelAtual);
+         NivelAtual++;
          ProximaQuestao();
+        }
+        else
+        {
+          await App.Current.MainPage.DisplayAlert("Game Over", "Você perdeu!", "ok");
+          Inicializar();
         }
       }
 }
